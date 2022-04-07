4289ab3aa6de87ae46603ee745ec520f528ee2d8
nidhesh pandey <nidhesh@live.com>
2022-04-07 11:47:34 +0530

    Add project files.

---
+++ FirstWebApi/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstWebApi.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderCreateDate { get; set; }
+        public bool IsActive { get; set; }
+        public int OrderStatus { get; set; }
+    }
+}
